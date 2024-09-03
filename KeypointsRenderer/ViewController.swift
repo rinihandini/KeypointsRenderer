e5d9c4366f39f02b09de1d7dcc0759f4343fddb9
Rini Handini <riniraikkonen@yahoo.co.id>
2024-09-03 13:08:35 +0800

    Initial Commit

---
+++ KeypointsRenderer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeypointsRenderer
+//
+//  Created by Rini Handini on 03/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
